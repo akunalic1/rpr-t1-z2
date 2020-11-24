3201454dfbea23e2a328498d9cd574d23425a496
Amira Kunalic <akunalic1@etf.unsa.ba>
2020-11-24 11:42:20 +0100

    dodajStudentaNaSpisak

--- stub projekat/stub projekat/src/ba/unsa/etf/rpr/t5/PlanStudija.java
+++
@@ -1,9 +0,0 @@
-package ba.unsa.etf.rpr.t5;
-
-import java.util.ArrayList;
-
-public class PlanStudija {
-    public Integer ciklus;
-    public ArrayList<Predmet> spisakDozvoljenihIzbornihPredmeta;
-    public ArrayList<Predmet> spisakObaveznihPredmeta;
-}
